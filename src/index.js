e71514d0b3e5a21c0dd120fe949b585f128b2382
Francisco Cortez <franciscocortezdev@gmail.com>
2022-11-23 19:55:37 -0600

    Routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { dirname, join } from 'path'
 import { fileURLToPath } from 'url'
+import router from './routes/index.js'
 
 const app = express()
 
@@ -8,18 +9,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set('views', join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(router)
 
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('/contact', (req, res) => {
-  res.render('contact')
-})
 
 
 app.listen(3000, () => {
